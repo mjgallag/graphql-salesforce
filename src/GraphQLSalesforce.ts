9799555eef9458c8b432c8d63119837eb8de7766
Michael J Gallagher <mjgallag@gmail.com>
2016-11-14 14:57:51 -0800

    Implement 'SELECT Name FROM Account LIMIT 1' with jsforce

--- src/GraphQLSalesforce.ts
+++ src/GraphQLSalesforce.ts
@@ -1,5 +1,10 @@
+import { Connection } from 'jsforce';
+
 export default class GraphQLSalesforce {
-  static helloWorld(): void {
-    console.log('Hello World!');
+  static async helloWorld(username: string, password: string): Promise<void> {
+    const connection = new Connection();
+
+    await connection.login(username, password);
+    console.log(await connection.query('SELECT Name FROM Account LIMIT 1'));
   }
 }
