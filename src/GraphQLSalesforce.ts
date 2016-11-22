e1301f1194bfcb13e3cf8517c42bb0b5043af2f7
Michael J Gallagher <mjgallag@gmail.com>
2016-11-22 11:23:58 -0800

    Implement sObject & field autocomplete with graphql

--- src/GraphQLSalesforce.ts
+++ src/GraphQLSalesforce.ts
@@ -1,10 +1,49 @@
+import {
+  GraphQLFieldConfigMap,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString
+} from 'graphql';
 import { Connection } from 'jsforce';
 
 export default class GraphQLSalesforce {
-  static async helloWorld(username: string, password: string): Promise<void> {
-    const connection = new Connection();
+  private static connection = new Connection();
 
-    await connection.login(username, password);
-    console.log(await connection.query('SELECT Name FROM Account LIMIT 1'));
+  static async makeSchema(username: string, password: string): Promise<GraphQLSchema> {
+    await GraphQLSalesforce.connection.login(username, password);
+
+    return new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Salesforce',
+        fields: await GraphQLSalesforce.makeObjectTypes()
+      })
+    });
+  }
+
+  private static async makeObjectTypes(): Promise<GraphQLFieldConfigMap<any>> {
+    const objectTypes: GraphQLFieldConfigMap<any> = {};
+
+    for (const sObject of (await GraphQLSalesforce.connection.describeGlobal()).sobjects)
+      objectTypes[sObject.name] = {
+        type: new GraphQLObjectType({
+          name: sObject.name,
+          fields: await GraphQLSalesforce.makeScalarTypes(sObject.name)
+        }),
+        description: sObject.label
+      };
+
+    return objectTypes;
+  }
+
+  private static async makeScalarTypes(sObject: string): Promise<GraphQLFieldConfigMap<any>> {
+    const scalarTypes: GraphQLFieldConfigMap<any> = {};
+
+    for (const field of (await GraphQLSalesforce.connection.sobject(sObject).describe()).fields)
+      scalarTypes[field.name] = {
+        type: GraphQLString,
+        description: field.label
+      };
+
+    return scalarTypes;
   }
 }
